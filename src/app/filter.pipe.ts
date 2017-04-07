3d1575afd4c9de675daa82d7d68b958f1282ee55
Minh Phuong <mphuong@kent.edu>
2017-04-07 12:17:11 -0700

    add content to filter pipe

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,12 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { BookService } from './book.service';
 
 @Pipe({
   name: 'filter'
 })
+
 export class FilterPipe implements PipeTransform {
+  bookType:string;
 
-  transform(value: any, args?: any): any {
-    return null;
+  constructor(private bookService: BookService, private af:AngularFire){}
+
+  transform(value: any, bookType){
+    if(bookType != "all"){
+      var filteredBooks = this.af.database.list('books',{
+        query:{
+          orderByChild:'genre',
+          equalTo: bookType
+        }
+      });
+      return filteredBooks;
+    } else {
+      return this.af.database.list('books');
+    }
   }
-
 }
