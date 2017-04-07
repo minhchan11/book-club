7e19d6061490a79f67041c05e14d2dee1914a435
Minh Phuong <mphuong@kent.edu>
2017-04-07 08:48:25 -0700

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FilterPipe } from './filter.pipe';
+import { BooksComponent } from './books/books.component';
+import { BookComponent } from './book/book.component';
+import { BookNewComponent } from './book-new/book-new.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe,
+    BooksComponent,
+    BookComponent,
+    BookNewComponent,
+    BookEditComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
