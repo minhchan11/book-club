1933ac02fcb49dbd0ac8f63d062d2ddc632422cb
Minh Phuong <mphuong@kent.edu>
2017-04-07 09:32:33 -0700

    set up first service

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,1 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Book } from './book.model';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
+@Injectable()
+export class BookService {
+  books: FirebaseListObservable<any[]>;
+
+  constructor(private af: AngularFire) {
+    this.books = af.database.list('books');
+  }
+
+  getBooks(){
+    return this.books;
+  }
+
+}
